547e0c2df6abfbe664c83f9b7330f458f0c519fc
Sonder-design <egorkursedskij@gmail.com>
2024-10-12 21:53:06 +0300

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,22 @@
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------|
+# EN: 
+#     - Do not touch or modify the code below. If there is an error, please contact the owner, but under no circumstances should you touch the code.
+#     - Do not resell this tool, do not credit it to yours.
+# FR: 
+#     - Ne pas toucher ni modifier le code ci-dessous. En cas d'erreur, veuillez contacter le propriétaire, mais en aucun cas vous ne devez toucher au code.
+#     - Ne revendez pas ce tool, ne le créditez pas au vôtre.
+
+name_tool       =  "Katra Tool"
+type_tool       =  "Multi-Tools"
+version_tool    =  "2.0"
+coding_tool     =  "Python 3"
+language_tool   =  "EN"
+creator         =  "deviceattcker"
+platform        =  "Windows 10/11 & Linux"
+discord_server  =  "darkkatra"
+telegram        =  "t.me/darkkatra"
+github_tool     =  ""
+url_config      =  "https://raw.githubusercontent.com/loxy0dev/RedTiger-Tools/main/Settings/Program/Config/Config.py"
+url_downloads   =  "https://github.com/loxy0dev/RedTiger-Tools/archive/main.zip"
+license         =  "https://github.com/loxy0dev/RedTiger-Tools/blob/main/LICENSE"
+copyright       =  "Copyright (c) RedTiger 'GPL-3.0 license'"
